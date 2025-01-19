9d75bbd83d966d50d1a8ac48b2d3f6b871b7f55b
Beatle <123680498+DavidCoderror@users.noreply.github.com>
2025-01-19 14:51:48 -0500

    ADDED: Function 4: Inversion

--- pointers.cpp
+++ pointers.cpp
@@ -4,10 +4,15 @@ using namespace std;
 //Function 1: String 1 + String 2
 //Function 2: Recherche d'un char
 //Function 3: Recherche nombre occurence de char dans string
-
+//Function 4: Inversion du phrase
 
 //--------------------------------------------////--------------------------------------------//
-
+//--------------------------------------------////--------------------------------------------//
+//--------------------------------------------////--------------------------------------------//
+//------------------------------------------Functions-----------------------------------------//
+//--------------------------------------------////--------------------------------------------//
+//--------------------------------------------////--------------------------------------------//
+//--------------------------------------------////--------------------------------------------//
 char* conc(char* str1, char* str2) { // String 1 + String 2
 
     // Length of String 1
@@ -76,6 +81,31 @@ char* occurence(char* str, char c) { // Recherche nombre occurence de char dans 
 }
 
 //--------------------------------------------////--------------------------------------------//
+
+char* inversion(char* str) { // Inversion du phrase
+    int i = 0;
+    int j = 0;
+    char* resultat = new char[100];
+
+    while (str[j] != '\0') { j++; } //Longueur du phrase
+    j--; //(Pour eviter Erreur Out of Index)
+
+    while (i < j) {
+        char temp = str[i]; //Temporaire pour prendre valeur debut
+        str[i] = str[j]; // Le valeur au debut prend celle au dernier
+        str[j] = temp; //Le valeur au fin prend le valeur du premier car du temp
+        i++; //i bouge aux droit, 
+        j--; //j bouge aux gauche,
+
+        //Il continue jusqu'il rencontre aux milieu
+    }
+
+    snprintf(resultat, 100, "Ton phrase a ete inverser et est maintenant << %s <<.", str);
+    return resultat;
+}
+
+//--------------------------------------------////--------------------------------------------//
+//--------------------------------------------////--------------------------------------------//
 //--------------------------------------------////--------------------------------------------//
 //--------------------------------------------MAIN--------------------------------------------//
 //--------------------------------------------////--------------------------------------------//
@@ -87,9 +117,10 @@ int main()
     char str1[] = "Hello";
     char str2[] = "World";
 
-     char* resultat = conc(str1,str2);
-    // char* resultat = recherche(str1,'o');
+    //char* resultat = conc(str1,str2);
+    //char* resultat = recherche(str1,'o');
     //char* resultat = occurence(str1, 'o');
+     char* resultat = inversion(str1);
 
     cout << "String: " << resultat << endl;
 
